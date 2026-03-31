a69c6273735a5e7cd96a96c2a850f9be71b65ed8
Nguyễn Đăng Thành <dthanhh811@gmail.com>
2026-03-31 10:02:14 +0700

    Time: 35 ms (89.2%), Space: 38.2 MB (18%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.c
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.c
@@ -1,27 +1,37 @@
-int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize) 
-{
-    int cmp(const void *a, const void *b)
-    {
-        int **x = (int **)a;
-        int **y = (int **)b;
-        if(*(*x + 1) < *(*y + 1)) 
-            return -1;
-        else
-            return 1;
-    }
+typedef struct {
+    int start;
+    int end;
+} Interval;
 
-    qsort(intervals, intervalsSize, sizeof(int *), cmp);
-
-    int ans = 1;
-    int last_end = intervals[0][1];
-    for(int i = 1; i < intervalsSize; i++)
-    {
-        if(intervals[i][0] >= last_end)
-        {
-            ans ++;
-            last_end = intervals[i][1];
-        }
-    }
-    return intervalsSize - ans;
+int compare(const void *a, const void *b) {
+    const Interval *i1 = (const Interval *)a;
+    const Interval *i2 = (const Interval *)b;
+    if (i1->end < i2->end) return -1;
+    if (i1->end > i2->end) return 1;
+    return 0;
 }
 
+int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize) {
+    if (intervalsSize <= 1) return 0;
+
+    Interval *flat = (Interval *)malloc(intervalsSize * sizeof(Interval));
+    for (int i = 0; i < intervalsSize; i++) {
+        flat[i].start = intervals[i][0];
+        flat[i].end = intervals[i][1];
+    }
+
+    qsort(flat, intervalsSize, sizeof(Interval), compare);
+
+    int count = 1; 
+    int last_end = flat[0].end;
+
+    for (int i = 1; i < intervalsSize; i++) {
+        if (flat[i].start >= last_end) {
+            count++;
+            last_end = flat[i].end;
+        }
+    }
+
+    free(flat);
+    return intervalsSize - count;
+}
