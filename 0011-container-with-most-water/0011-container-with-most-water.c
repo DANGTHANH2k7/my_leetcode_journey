8c046aad70192af91deafd713cda7c3236c387e2
Nguyễn Đăng Thành <dthanhh811@gmail.com>
2026-04-09 22:40:32 +0700

    Time: 8 ms (5.21%), Space: 14.8 MB (6.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.c
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -1,3 +1,17 @@
-int maxArea(int* height, int heightSize) {
-    
+int maxArea(int* height, int heightSize)
+{
+    int n = heightSize;
+    int r = 0, l = n-1, maxV = 0;
+    while(r < l)
+    {
+        int hr = height[r];
+        int hl = height[l];
+        int len = l-r;
+        int v = len*fmin(hl, hr);
+        maxV = fmax(maxV, v);
+        if(hr < hl) r++;
+        else if(hr > hl) l--;
+        else r++, l--;
+    }
+    return maxV;
 }
