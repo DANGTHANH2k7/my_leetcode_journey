2488f0369c4c7c4adb258400ff7c5c9e9226a5e5
Nguyễn Đăng Thành <dthanhh811@gmail.com>
2026-04-14 18:07:53 +0700

    Time: 0 ms (100%), Space: 8.9 MB (26.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.c
+++ 0198-house-robber/0198-house-robber.c
@@ -1,42 +1,19 @@
-int memo[100];
-
-int dp(int* nums, int i)
-{
-    if(i == 0)
-    {   
-        memo[0] = nums[0];
-        return nums[0];
-    }
-    if(i == 1)
-    {   
-        memo[1] = nums[1];
-        return nums[1];
-    } 
-
-    if(memo[i] != -1) return memo[i];
-
-    int max_i = nums[i];
-    for(int j = 0; j <= i-2; j++)
-    {
-        max_i = fmax(max_i, dp(nums, j) + nums[i]);
-    }
-    memo[i] = max_i;
-
-    return memo[i];
-}
-
 int rob(int* nums, int numsSize) 
 {
-    memset(memo, -1, sizeof(memo));
-    for(int i = 0; i < numsSize; i++)
+    if (numsSize == 0) return 0;
+    if (numsSize == 1) return nums[0];
+
+    int prev2 = 0; 
+    int prev1 = 0; 
+    int current = 0;
+
+    for (int i = 0; i < numsSize; i++) 
     {
-        dp(nums, i);
+        current = fmax(prev1, nums[i] + prev2);
+
+        prev2 = prev1;
+        prev1 = current;
     }
-    int res = -1;
-    for(int i = 0; i < numsSize; i++)
-    {
-        res = fmax(res, memo[i]);
-    }
-    
-    return res;
+
+    return prev1;
 }
