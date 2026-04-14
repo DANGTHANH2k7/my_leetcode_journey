c4c7817cef3057356f395887282b73111ab3f37d
Nguyễn Đăng Thành <dthanhh811@gmail.com>
2026-04-14 22:30:19 +0700

    Time: 135 ms (5.07%), Space: 9.1 MB (23.43%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.c
@@ -0,0 +1,36 @@
+int memo[2501];
+
+int dp(int* nums, int i)
+{
+    if(i == 0) 
+    {
+        memo[0] = 1;
+        return 1;
+    }
+
+    if(memo[i] != 0) return memo[i];    
+
+    int maxi = 1;
+    for(int j = 0; j < i; j++)
+    {
+        if(nums[j] < nums[i])
+        {   
+            maxi = fmax(maxi, dp(nums, j) + 1);
+        }
+    }
+    memo[i] = maxi;
+
+    return memo[i];
+}
+
+int lengthOfLIS(int* nums, int numsSize) 
+{
+    memset(memo, 0, sizeof(memo));
+    dp(nums, numsSize-1);
+    int res = -1;
+    for(int i = 0; i < numsSize; i++)
+    {
+        res = fmax(res, dp(nums, i));
+    }
+    return res;
+}
