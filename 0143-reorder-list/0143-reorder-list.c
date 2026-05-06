4eaff34b0814cba0471370a2be6cc3e0b0eb9231
Nguyễn Đăng Thành <dthanhh811@gmail.com>
2026-05-06 22:33:56 +0700

    Time: 0 ms (100%), Space: 13.9 MB (100%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.c
+++ 0143-reorder-list/0143-reorder-list.c
@@ -1,18 +1,14 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
 struct ListNode* reverse(struct ListNode* head)
+
 {
     typedef struct ListNode node;
+    
     if(head == NULL || head->next == NULL) return head;
+
     node* pre = NULL;
     node* cur = head;
     node* nex = cur->next;
+
     while(cur != NULL)
     {
         nex = cur->next;
@@ -20,18 +16,35 @@ struct ListNode* reverse(struct ListNode* head)
         pre = cur;
         cur = nex;
     }
+    
     return pre;
 }
 
 void reorderList(struct ListNode* head) 
 {
+    if (!head || !head->next || !head->next->next) return;
+
     typedef struct ListNode node;
-    node* cur = head;
-    node* newHead = reverse(cur->next);
-    while(newHead != NULL)
+
+    node *slow = head, *fast = head;
+    while (fast->next && fast->next->next) 
     {
-        cur->next = newHead;
-        cur = cur->next;
-        newHead = reverse(cur->next);
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    node *second = reverse(slow->next);
+    slow->next = NULL; 
+
+    node *first = head;
+    while (second) 
+    {
+        node *tmp1 = first->next;
+        node *tmp2 = second->next;
+
+        first->next = second;
+        second->next = tmp1; 
+        first = tmp1;
+        second = tmp2;
     }
 }
